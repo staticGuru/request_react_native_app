d788cc548427b95a2fcd59f6470e0d8407b26074
staticGuru <vigneshguru274@gmail.com>
2023-01-15 18:59:46 +0530

    add the lint and prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-/* eslint-disable no-unused-vars */
-import 'react-native-gesture-handler' 
+import 'react-native-gesture-handler';
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 
 import Routes from './routes';
 
 const App = () => {
-  return <NavigationContainer><Routes/></NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
 };
 
 export default App;
